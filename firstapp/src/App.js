f5237ec678db89ceec9cdfcc3e0f89c3f7926eaf
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-29 13:54:31 +0200

    react props

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import './App.css';
-import {Container} from 'react-bootstrap'
-import Footer from './Compnents/Footer';
-import Header from './Compnents/Header';
+import { Container } from 'react-bootstrap';
+
+const Hello = (props) =>{
+  return(
+    <div>
+      <p> Hello {props.name}, has job {props.job} </p>
+
+    </div>
+  );
+}
 
 function App() {
-  /** The return statement of React component can only return one element.
-   * In the real life, you will often have multiple elements and then you have to wrap these inside one element like div.
-   * We can also use  <React.Fragment> */
-  /** for inline style we use camel casing like  */
+  const now = new Date();
+  const a=10;
+  const b=15;
   return (
-    /*<div style={{fontSize: '10px'}}> 
-      <h1> Hello React</h1>
-      <p>we are learning React</p>
-      <button>Press Me</button>
-    </div> The better idea is to use classes for style*/
-
-    <>
-      <Header />
-      <Container>
-      <mian>
-        <h1 className='my-4'>Welcome to React Toturial</h1>
-      </mian>
-      </Container>
-     
-      <Footer />
-    </>
+    <Container>
+      <div>
+      <h1>Hello React</h1>
+      <p>The Current time is, {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
+      <Hello name=' Atti' job='Engineer'/>
+      <Hello name='Ahmad' job='Developer'/>
+      </div>
+    </Container>
   );
 }
 
