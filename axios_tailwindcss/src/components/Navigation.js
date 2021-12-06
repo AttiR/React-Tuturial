3a2ff71785de9c214260a41aeccff770bbe321d4
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-06 14:33:05 +0200

    menu bar

--- axios_tailwindcss/src/components/Navigation.js
+++ axios_tailwindcss/src/components/Navigation.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBars} from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => {
-    return (
-        <nav>
-        <span className='text-xl'>
-        <FontAwesomeIcon
-                icon={ faBars }
-            />
-        </span>
-           
-            
-        </nav>
-    )
-}
+  const [showMenu, setShowMenu] = useState(false)
 
-export default Navigation
+  let menu
+
+  if (setShowMenu) {
+    menu = <div> The Menu </div>;
+  }
+  return (
+    <nav>
+      <span className="text-xl">
+        <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
+      </span>
+      {menu}
+    </nav>
+  );
+};
+
+export default Navigation;
