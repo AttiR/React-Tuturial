9ea22b6006c62440bf2d5d83c9c2c872f45a480a
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-09 09:02:36 +0200

    Registration form

--- hookform/src/App.js
+++ hookform/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div>
-      <Container className="p-5">
-        <Login />
+      <Container>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </Container>
     </div>
   );
---
+++ hookform/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <center>
+        {' '}
+        <h2 className="py-5"> React Hooks-form</h2>
+      </center>
+    </div>
+  );
+};
+
+export default Home;
