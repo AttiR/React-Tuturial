3e75fe39824bbc3bc8c8a6cb14e6363a8202e1c0
AttiR <77799634+AttiR@users.noreply.github.com>
2021-12-16 10:26:41 +0200

    modified crud operation bookstore

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -5,20 +5,23 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import Heading from './components/Heading';
 import AddBook from './components/AddBook';
+import EditBook from './components/EditBook';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [id, setId] = useState();
+
+  const { REACT_APP_API, REACT_APP_DELETE_API } = process.env;
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
   const fetchBooks = () => {
-    fetch(
-      'API'
-    )
+    fetch(`${REACT_APP_API}`)
       .then((response) => response.json())
       .then((data) => addKeys(data))
       .catch((err) => console.error(err));
@@ -35,13 +38,20 @@ function App() {
 
   // Post Request for add Book
   const addBook = (newBook) => {
-    fetch(
-      'API',
-      {
-        method: 'POST',
-        body: JSON.stringify(newBook),
-      }
-    )
+    fetch(`${REACT_APP_API}`, {
+      method: 'POST',
+      body: JSON.stringify(newBook),
+    })
+      .then((response) => fetchBooks())
+      .catch((err) => console.error(err));
+  };
+
+  // Eit Book
+  const changeBook = (id, book) => {
+    fetch(`REACT_APP_DELETE_API}`, {
+      method: 'PUT',
+      body: JSON.stringify(book),
+    })
       .then((response) => fetchBooks())
       .catch((err) => console.error(err));
   };
@@ -49,12 +59,9 @@ function App() {
   // Post Request to Delete Book
 
   const deleteBook = (id) => {
-    fetch(
-      `DEL_API`,
-      {
-        method: 'DELETE',
-      }
-    )
+    fetch(`${REACT_APP_DELETE_API}`, {
+      method: 'DELETE',
+    })
       .then((response) => fetchBooks())
       .catch((err) => console.error(err));
   };
@@ -66,17 +73,33 @@ function App() {
         <center className="py-5">
           <AddBook addBook={addBook} />
         </center>
+        <EditBook id={id} changeBook={changeBook} />
 
         <div
           className="ag-theme-material"
-          style={{ height: 400, width: 1200, margin: 'auto' }}
+          style={{ height: 400, width: 1000, margin: 'auto' }}
         >
           <AgGridReact rowData={books}>
             <AgGridColumn sortable={true} filter={true} field="title" />
             <AgGridColumn sortable={true} filter={true} field="author" />
-            <AgGridColumn sortable={true} filter={true} field="year" />
-            <AgGridColumn sortable={true} filter={true} field="isbn" />
-            <AgGridColumn sortable={true} filter={true} field="price" />
+            <AgGridColumn  width={100} sortable={true} filter={true} field="year" />
+            <AgGridColumn  sortable={true} filter={true} field="isbn" />
+            <AgGridColumn width={100} sortable={true} filter={true} field="price" />
+
+            <AgGridColumn
+              headerName=""
+              field="id"
+              width={90}
+              cellRendererFramework={(params) => (
+                <IconButton
+                  onClick={() => setId(params.value)}
+                  size="small"
+                  color="primary"
+                >
+                  <EditIcon />
+                </IconButton>
+              )}
+            />
 
             <AgGridColumn
               headerName=""
